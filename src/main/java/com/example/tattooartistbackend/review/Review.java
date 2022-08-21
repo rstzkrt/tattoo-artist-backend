b53ee1048d2951f95be021220d45f6d32686ac01
resat <ozkurtresat@gmail.com>
2022-08-21 16:01:39 +0200

    add review crud impl

--- src/main/java/com/example/tattooartistbackend/review/Review.java
+++ src/main/java/com/example/tattooartistbackend/review/Review.java
@@ -1,6 +1,48 @@
 package com.example.tattooartistbackend.review;
 
-import javax.persistence.Entity;
+import com.example.tattooartistbackend.generated.models.ReviewPostRequestDto;
+import com.example.tattooartistbackend.generated.models.ReviewResponseDto;
+import com.example.tattooartistbackend.generated.models.ReviewType;
+import com.example.tattooartistbackend.user.User;
+import lombok.*;
 
-public class Review { //for the tattoo artist everybody will be able to send review {optional}
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private ReviewType reviewType;
+    private String message;
+    @ManyToOne
+    private User postedBy ;
+    @ManyToOne
+    private User receiver;
+
+    public ReviewResponseDto toReviewResponseDto(){
+        ReviewResponseDto reviewResponseDto=new ReviewResponseDto();
+        reviewResponseDto.setReviewType(reviewType);
+        reviewResponseDto.setMessage(message);
+        reviewResponseDto.setId(id);
+        reviewResponseDto.setReceiver(receiver.getId());
+        reviewResponseDto.setPostedBy(postedBy.getId());
+        return  reviewResponseDto;
+    }
+
+    public static Review fromReviewResponseDto(ReviewPostRequestDto reviewPostRequestDto, User postedBy, User receiver){
+        return Review.builder()
+                .reviewType(reviewPostRequestDto.getReviewType())
+                .message(reviewPostRequestDto.getMessage())
+                .postedBy(postedBy)
+                .receiver(receiver)
+                .build();
+    }
 }
