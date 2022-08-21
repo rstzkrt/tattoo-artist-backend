4bb47bb8804df362e4829e1fb3c9d509dd57eb4f
resat <ozkurtresat@gmail.com>
2022-08-21 20:03:53 +0200

    add new exception handling for media types

--- src/main/java/com/example/tattooartistbackend/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/tattooartistbackend/exceptions/GlobalExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.example.tattooartistbackend.exceptions;
 
 import com.example.tattooartistbackend.generated.models.ErrorResponse;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -18,7 +20,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
             TattooWorkNotFoundException.class,
             AddressNotFoundException.class,
             CommentNotFoundException.class,
-            ReviewNotFoundException.class
+            ReviewNotFoundException.class,
     })
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(RuntimeException exception, WebRequest webRequest) {
@@ -42,5 +44,13 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         errorResponse.setCode(HttpStatus.METHOD_NOT_ALLOWED.value());
         errorResponse.setMessage(exception.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.METHOD_NOT_ALLOWED);
+    }
+    @Override
+    @ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+    public ResponseEntity<Object> handleHttpMediaTypeNotSupported(HttpMediaTypeNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setCode(status.value());
+        errorResponse.setMessage(ex.getMessage());
+        return new ResponseEntity<>(errorResponse, status);
     }
 }
