8703bcdcd7f06152b7e4dc3f0b78e09913a50af1
resat <ozkurtresat@gmail.com>
2022-12-02 20:09:49 +0100

    classpath modified

--- src/main/java/com/example/tattooartistbackend/configuration/FirebaseConfig.java
+++ src/main/java/com/example/tattooartistbackend/configuration/FirebaseConfig.java
@@ -14,26 +14,27 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.ClassPathResource;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 
 @Configuration
 @RequiredArgsConstructor
 public class FirebaseConfig {
+
     private final SecurityProperties secProps;
-    private static final String FIREBASE_CONFIG_FILE_PATH="src/main/resources/firebase/firebase_config.json";
+//    private static final String FIREBASE_CONFIG_FILE_PATH="classpath:firebase_config.json";
     @Primary
     @Bean
     public FirebaseApp getFirebaseApp() throws IOException {
-        FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(new FileInputStream(FIREBASE_CONFIG_FILE_PATH)))
+        var resource = new ClassPathResource("firebase_config.json");
+        var options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(resource.getInputStream()))
                 .setDatabaseUrl(secProps.getFirebaseProps().getDatabaseUrl())
                 .build();
         if (FirebaseApp.getApps().isEmpty()) {
             FirebaseApp.initializeApp(options);
         }
-
         return FirebaseApp.getInstance();
     }
 
@@ -49,8 +50,9 @@ public class FirebaseConfig {
 
     @Bean
     public Firestore getDatabase() throws IOException {
-        FirestoreOptions firestoreOptions = FirestoreOptions.newBuilder()
-                .setCredentials(GoogleCredentials.fromStream(new FileInputStream(FIREBASE_CONFIG_FILE_PATH)))
+        var resource = new ClassPathResource("firebase_config.json");
+        var firestoreOptions = FirestoreOptions.newBuilder()
+                .setCredentials(GoogleCredentials.fromStream(resource.getInputStream()))
                 .build();
         return firestoreOptions.getService();
     }
